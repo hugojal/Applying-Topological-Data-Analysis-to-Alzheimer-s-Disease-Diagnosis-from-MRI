30b55b2cc6616d6daea7121ba59938b297541172
RaviShah2011 <153964419+RaviShah2011@users.noreply.github.com>
2024-01-02 17:42:21 -0600

    Create data_loading.c

---
+++ data_loading.c
@@ -0,0 +1,33 @@
+(If you are running this on google colab):
+
+
+! mkdir ~/.kaggle
+
+! cp kaggle.json ~/.kaggle/
+
+! chmod 600 ~/.kaggle/kaggle.json
+
+! kaggle datasets download jboysen/mri-and-alzheimers
+
+# Put on the same directory
+from zipfile import ZipFile
+
+
+# specifying the name of the zip file
+file = "/content/archive (6).zip"
+
+
+# open the zip file in read mode
+with ZipFile(file, 'r') as zip:
+    # list all the contents of the zip file
+    zip.printdir()
+
+
+    # extract all files
+    print('extraction...')
+    zip.extractall()
+    print('Done!')
+
+import pandas as pd
+
+df = pd.read_csv('/content/oasis_longitudinal.csv')
