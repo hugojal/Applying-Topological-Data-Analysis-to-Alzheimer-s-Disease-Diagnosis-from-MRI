ccdbc4864cc0f721b39bba123bb3700f35de0a64
RaviShah11 <140932723+RaviShah11@users.noreply.github.com>
2024-04-05 18:13:52 -0500

    Update Stochastic TDA.py

--- Stochastic TDA.py
+++ Stochastic TDA.py
@@ -15,3 +15,71 @@ fig = plt.figure(figsize=(10, 2))
 grid = ImageGrid(fig, 111,nrows_ncols=(1, num_disp),axes_pad=0.1)
 for ax,x in zip(grid, X[:num_disp]):
     ax.imshow(x.reshape(8,8),cmap="gray")
+
+
+
+
+pca = PCA(n_components=3)
+X_pca = pca.fit(X).transform(X)
+# plot first and second primary components
+plt.scatter(X_pca[:,0],X_pca[:,1],c=Y)
+plt.xlabel("PC1")
+plt.ylabel("PC2")
+plt.show()
+
+
+
+
+X_tsne = manifold.TSNE(n_components=2,perplexity=20).fit_transform(X)
+plt.scatter(X_tsne[:,0],X_tsne[:,1],c=Y)
+
+
+
+
+
+import umap
+X_umap = umap.UMAP(n_components=2, n_neighbors=5, metric='euclidean').fit_transform(X)
+plt.scatter(X_umap[:,0],X_umap[:,1],c=Y)
+
+
+
+
+
+import kmapper, kmapper.jupyter
+from sklearn.preprocessing import StandardScaler
+import networkx as nx
+
+km = kmapper.KeplerMapper()
+X_std = StandardScaler().fit_transform(X)   # standardise
+f = km.project(X_std,projection="sum") # we have to choose a 1-dim projection
+graph = km.map(lens=f, X=X_std, clusterer=sklearn.cluster.DBSCAN(metric="cosine",algorithm='auto', eps=0.3, leaf_size=10))
+nx.draw(kmapper.adapter.to_nx(graph))
+#km.visualize(graph, color_function=Y, custom_tooltips=Y, path_html="kepler-mapper-output.html") # To download the output, look at the beginning of this note
+km.visualize(graph, custom_tooltips=Y, path_html="kepler-mapper-output.html") # To download the output, look at the beginning of this note
+print("Download kepler-mapper-output.html and open it with a browser to see the results.")
+
+
+
+
+
+pim = pimg.PersistenceImager(birth_range=(0,3), pers_range=(0,3), pixel_size=0.5)
+pds, pims_h0, pims_h1 = [], [], []
+for i in range(len(X)):
+    img = X[i].reshape(8,8)
+    bw_img = (img >= threshold_otsu(img))
+    dt_img = distance_transform_edt(~bw_img)
+    pd = cripser.computePH(dt_img)    # compute PH of distance transform
+    pd[:,1:3] = np.clip(pd[:,1:3],a_min=0,a_max=3) # clip min/max birth/death
+    pds.append(pd)
+    pim0 = pim.transform(pd[pd[:,0]==0,1:3])
+    pim1 = pim.transform(pd[pd[:,0]==1,1:3])
+    pims_h0.append(pim0.ravel()) # vectorise by persistence image
+    pims_h1.append(pim1.ravel()) # vectorise by persistence image
+
+(trainX, testX, trainY, testY) = train_test_split(pims_h1, Y, test_size = 0.3, random_state = 0)
+clf = RandomForestClassifier(max_depth=2, random_state=0)
+clf.fit(trainX, trainY)
+
+predY = clf.predict(testX)
+print("Confusion matrix\n",confusion_matrix(testY,predY))
+print(classification_report(testY,predY))
